3f4b450931e49277f532c13d0912d789f26497f8
EgzonGa <74424981+EgzonGa@users.noreply.github.com>
2021-05-16 22:21:31 +0200

    admindashboard & login/register*admin

---
+++ admins/header.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+
+<body>
+    <header class="header">
+        <a href="../home.php" class="logo">Dashboard Admin</a>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        <ul class="menu">
+
+            <li> <a href="#"><i class="fas fa-home"></i>Home</a></li>
+            <li> <a href="/addNewAdmin.php"><i class="fas fa-user-circle"></i>Admins</a> </li>
+            <li> <a href="#"><i class="fas fa-image"></i>#</a> </li>
+            <li> <a href="#"><i class="fas fa-newspaper"></i>#</a> </li>
+            <li> <a href="#"><i class="fas fa-user-circle"></i>#</a> </li>
+            <li> <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a> </li>
+        </ul>
+    </header>
+
+
+
+
+
+</html>
+</body>
+
+</html>
